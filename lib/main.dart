f0671d50940e0a360b3bd6db8cd2e74507d0afda
Lopezlasso <antoniolasso2004@gmail.com>
2026-04-18 21:29:26 -0500

    Lopez Lasso Antonio

--- lib/main.dart
+++ lib/main.dart
@@ -605,6 +605,7 @@ class _DireccionPageState extends State<DireccionPage> {
     });
     try {
       // Replace with your actual API endpoint for Direccion
+      // Aqui modifico Lopez Lasso Antonio
       final response = await http.get(
         Uri.parse(
           'https://educaysoft.org/whatsapp6a/app/controllers/DireccionController.php?action=api',
